92c95ca4ed6a16e87a1ebd3eef4afd04b8eeab4f
anakin200a-ai <anakin200a@gmail.com>
2026-06-19 16:30:08 +0300

    updates

--- routes/api/guest.php
+++ routes/api/guest.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\Api\Guest\GuestRestaurantController;
 use App\Http\Controllers\Api\Guest\GuestMenuCategoryController;
 use App\Http\Controllers\Api\Guest\GuestMenuItemController;
 
-Route::prefix('guest')->group(function () {
-    Route::get('/restaurants-guest', [GuestRestaurantController::class, 'index']);
-    Route::get('/restaurants-guest/{id}', [GuestRestaurantController::class, 'show']);
+// Route::prefix('guest')->group(function () {
+//     Route::get('/restaurants-guest', [GuestRestaurantController::class, 'index']);
+//     Route::get('/restaurants-guest/{id}', [GuestRestaurantController::class, 'show']);
 
-    Route::get('/menu-categories-guest', [GuestMenuCategoryController::class, 'index']);
-    Route::get('/menu-categories-guest/{id}', [GuestMenuCategoryController::class, 'show']);
+//     Route::get('/menu-categories-guest', [GuestMenuCategoryController::class, 'index']);
+//     Route::get('/menu-categories-guest/{id}', [GuestMenuCategoryController::class, 'show']);
 
-    Route::get('/menu-items-guest', [GuestMenuItemController::class, 'index']);
-    Route::get('/menu-items-guest/{id}', [GuestMenuItemController::class, 'show']);
-});
-Route::prefix('contact-us')->group(function () {
-    Route::post('/guest', [GuestContactUsController::class, 'store']);
-});
+//     Route::get('/menu-items-guest', [GuestMenuItemController::class, 'index']);
+//     Route::get('/menu-items-guest/{id}', [GuestMenuItemController::class, 'show']);
+// });
+// Route::prefix('contact-us')->group(function () {
+//     Route::post('/guest', [GuestContactUsController::class, 'store']);
+// });
